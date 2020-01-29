7bac48ceac2741cafa894368ddb200f858209164
Rodinei de Jesus <rodineiguitar@gmail.com>
2020-01-29 11:13:58 -0300

    update icons mobile favicon, add logo

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,89 +1,129 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import Content from '../Content';
-import Footer from '../Footer';
+import Content from "../Content";
+import Footer from "../Footer";
 
-import './Login.scss';
-import { errorsMessage } from '../../helpers';
-import apis from '../../services/api';
-import { setUser, isLoggedIn } from '../../services/auth';
+import { errorsMessage } from "../../helpers";
+import apis from "../../services/api";
+import { setUser, isLoggedIn } from "../../services/auth";
+
+import logo from "./../../assets/images/logo.png";
+import "./Login.scss";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    url: "/"
+  };
 
-    state = {
-        email: '',
-        password: '',
-        url: '/'
-    };
-
-    componentDidMount() {
-        if (isLoggedIn()) {
-            window.location.href = this.state.url;
-        }
+  componentDidMount() {
+    if (isLoggedIn()) {
+      window.location.href = this.state.url;
     }
+  }
 
-    onSubmit = e => {
-        e.preventDefault();
-        if (this.state.email !== '' && this.state.password !== '') {
-            apis.getLogin(this.state.email, this.state.password)
-                .then(response => {
-                    if (response.data) {
-                        setUser(response.data);
-                        this.redirect('Login realizado com sucesso.', 300, this.state.url);
-                    } else {
-                        toast.info('Usuário ou senha inválidos, favor verificar');
-                    }
-                }).catch(function (error) {
-                    errorsMessage(error.response);
-                }).finally(() => console.log('end'));
-        }
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.state.email !== "" && this.state.password !== "") {
+      apis
+        .getLogin(this.state.email, this.state.password)
+        .then(response => {
+          if (response.data) {
+            setUser(response.data);
+            this.redirect("Login realizado com sucesso.", 300, this.state.url);
+          } else {
+            toast.info("Usuário ou senha inválidos, favor verificar");
+          }
+        })
+        .catch(function(error) {
+          errorsMessage(error.response);
+        })
+        .finally(() => console.log("end"));
     }
+  };
 
-    redirect(msg, timeout, url) {
-        toast.success(msg);
-        setTimeout(() => {
-            window.location.href = url;
-        }, timeout);
-    }
+  redirect(msg, timeout, url) {
+    toast.success(msg);
+    setTimeout(() => {
+      window.location.href = url;
+    }, timeout);
+  }
 
-    render() {
-        const { email, password } = this.state;
-        return (
-            <Content>
-                <form className="form-signin text-center" onSubmit={(e) => this.onSubmit(e)}>
-                    <img className="img-fluid mb-4" src="https://via.placeholder.com/570x75/007bff/00000?text=Rdn+Finance" alt="img" />
-                    <h1 className="h3 mb-3 font-weight-normal">Login - Controle de Finanças</h1>
-                    <div className="form-group">
-                        <label htmlFor="email" className="sr-only">E-mail</label>
-                        <div className="input-group mb-2">
-                            <div className="input-group-prepend">
-                                <div className="input-group-text"><i className="fas fa-envelope-square"></i></div>
-                            </div>
-                            <input type="email" id="email" name="email" className="form-control" placeholder="E-mail" value={this.state.email} required onChange={e => this.setState({ email: e.target.value })} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password" className="sr-only">Senha</label>
-                        <div className="input-group mb-2">
-                            <div className="input-group-prepend">
-                                <div className="input-group-text"><i className="fas fa-unlock-alt"></i></div>
-                            </div>
-                            <input type="password" id="password" name="password" className="form-control" placeholder="Senha" value={this.state.password} required onChange={e => this.setState({ password: e.target.value })} />
-                        </div>
-                    </div>
-                    <button
-                        className="btn btn-lg btn-primary btn-block"
-                        type="submit"
-                        disabled={!email || !password}>Entrar <i className="fas fa-sign-in-alt"></i></button>
-                        <Link to="/forget" className="nav-link">Esqueci minha senha</Link>
-                </form>
-                <Footer className={'footer'} />
-            </Content>
-        )
-    };
+  render() {
+    const { email, password } = this.state;
+    return (
+      <Content>
+        <form
+          className="form-signin text-center"
+          onSubmit={e => this.onSubmit(e)}
+        >
+          <img className="img-fluid mb-4" src={logo} alt="img" />
+          <h1 className="h3 mb-3 font-weight-normal">
+            Login - Controle de Finanças
+          </h1>
+          <div className="form-group">
+            <label htmlFor="email" className="sr-only">
+              E-mail
+            </label>
+            <div className="input-group mb-2">
+              <div className="input-group-prepend">
+                <div className="input-group-text">
+                  <i className="fas fa-envelope-square"></i>
+                </div>
+              </div>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="form-control"
+                placeholder="E-mail"
+                value={this.state.email}
+                required
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="sr-only">
+              Senha
+            </label>
+            <div className="input-group mb-2">
+              <div className="input-group-prepend">
+                <div className="input-group-text">
+                  <i className="fas fa-unlock-alt"></i>
+                </div>
+              </div>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="form-control"
+                placeholder="Senha"
+                value={this.state.password}
+                required
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+            </div>
+          </div>
+          <button
+            className="btn btn-lg btn-primary btn-block"
+            type="submit"
+            disabled={!email || !password}
+          >
+            Entrar <i className="fas fa-sign-in-alt"></i>
+          </button>
+          <Link to="/forget" className="nav-link">
+            Esqueci minha senha
+          </Link>
+        </form>
+        <Footer className={"footer"} />
+      </Content>
+    );
+  }
 }
 
 export default Login;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,128 +1,200 @@
-import React, { Component } from 'react';
-import { toast } from 'react-toastify';
-import Content from '../Content';
-import Footer from '../Footer';
+import React, { Component } from "react";
+import { toast } from "react-toastify";
+import Content from "../Content";
+import Footer from "../Footer";
 
-import './Register.scss';
-import { errorsMessage } from '../../helpers';
-import { isLoggedIn } from '../../services/auth';
-import apis from '../../services/api';
+import { errorsMessage } from "../../helpers";
+import { isLoggedIn } from "../../services/auth";
+import apis from "../../services/api";
+
+import logo from "./../../assets/images/logo.png";
+import "./Register.scss";
 
 class Register extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+    url: "/",
+    status: "",
+    message: ""
+  };
 
-    state = {
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
-        url: '/',
-        status: '',
-        message: '',
-    };
-
-    componentDidMount() {
-        if (isLoggedIn()) {
-            window.location.href = this.state.url;
-        }
+  componentDidMount() {
+    if (isLoggedIn()) {
+      window.location.href = this.state.url;
     }
+  }
 
-    onSubmit = e => {
-        e.preventDefault();
-        if (this.state.name !== '' && this.state.email !== '' && this.state.password !== '' && this.state.password_confirmation !== '') {
-            let body = {
-                name: this.state.name,
-                email: this.state.email,
-                password: this.state.password,
-                password_confirmation: this.state.password_confirmation
-            };
+  onSubmit = e => {
+    e.preventDefault();
+    if (
+      this.state.name !== "" &&
+      this.state.email !== "" &&
+      this.state.password !== "" &&
+      this.state.password_confirmation !== ""
+    ) {
+      let body = {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+        password_confirmation: this.state.password_confirmation
+      };
 
-            apis.postRegister(body)
-                .then(response => {
-                    if (response.data.status === 'success') {
-                        toast.success(response.data.message);
-                        this.setState({ 
-                            status: 'success',
-                            message: response.data.message,
-                            name: '',
-                            email: '',
-                            password: '',
-                            password_confirmation: '',
-                        });
-                    } else {
-                        toast.info('Erro ao tentar cadastrar, favor verificar');
-                        this.setState({ 
-                            status: 'success',
-                            message: 'Erro ao tentar cadastrar, favor verificar'
-                        });
-                    }
-                }).catch(function (error) {
-                    errorsMessage(error.response);
-                }).finally(() => console.log('end'));
-        }
+      apis
+        .postRegister(body)
+        .then(response => {
+          if (response.data.status === "success") {
+            toast.success(response.data.message);
+            this.setState({
+              status: "success",
+              message: response.data.message,
+              name: "",
+              email: "",
+              password: "",
+              password_confirmation: ""
+            });
+          } else {
+            toast.info("Erro ao tentar cadastrar, favor verificar");
+            this.setState({
+              status: "success",
+              message: "Erro ao tentar cadastrar, favor verificar"
+            });
+          }
+        })
+        .catch(function(error) {
+          errorsMessage(error.response);
+        })
+        .finally(() => console.log("end"));
     }
+  };
 
-    render() {
-        const { name, email, password, password_confirmation, status, message } = this.state;
+  render() {
+    const {
+      name,
+      email,
+      password,
+      password_confirmation,
+      status,
+      message
+    } = this.state;
 
-        return (
-            <Content>
-                <form className="form-signup text-center" onSubmit={(e) => this.onSubmit(e)}>
-                    <img className="img-fluid mb-4" src="https://via.placeholder.com/570x75/007bff/00000?text=Rdn+Finance" alt="img" />
-                    <h1 className="h3 mb-3 font-weight-normal">Registro - Controle de Finanças</h1>
-                    <div className={status !== '' ? `alert alert-${status}` : ``}>{message}</div>
-                    <div className="form-group">
-                        <label htmlFor="name" className="sr-only">Nome</label>
-                        <div className="input-group mb-2">
-                            <div className="input-group-prepend">
-                                <div className="input-group-text"><i className="fas fa-user"></i></div>
-                            </div>
-                            <input type="text" id="name" name="name" className="form-control" 
-                                value={name}
-                                placeholder="Nome" required onChange={e => this.setState({ name: e.target.value })} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email" className="sr-only">E-mail</label>
-                        <div className="input-group mb-2">
-                            <div className="input-group-prepend">
-                                <div className="input-group-text"><i className="fas fa-envelope-square"></i></div>
-                            </div>
-                            <input type="email" id="email" name="email" className="form-control" 
-                                value={email}
-                                placeholder="E-mail" required onChange={e => this.setState({ email: e.target.value })} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password" className="sr-only">Senha</label>
-                        <div className="input-group mb-2">
-                            <div className="input-group-prepend">
-                                <div className="input-group-text"><i className="fas fa-unlock-alt"></i></div>
-                            </div>
-                            <input type="password" id="password" name="password" className="form-control" 
-                                value={password}
-                                placeholder="Senha" required onChange={e => this.setState({ password: e.target.value })} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password" className="sr-only">Confirmar Senha</label>
-                        <div className="input-group mb-2">
-                            <div className="input-group-prepend">
-                                <div className="input-group-text"><i className="fas fa-unlock-alt"></i></div>
-                            </div>
-                            <input type="password" id="password_confirmation" name="password_confirmation" 
-                                value={password_confirmation}
-                                className="form-control" placeholder="Confirmar Senha" required onChange={e => this.setState({ password_confirmation: e.target.value })} />
-                        </div>
-                    </div>
-                    <button
-                        className="btn btn-lg btn-primary btn-block"
-                        type="submit"
-                        disabled={!name || !email || !password || !password_confirmation}>Cadastrar <i className="fas fa-user-plus"></i></button>
-                </form>
-                <Footer className={'footer'} />
-            </Content>
-        )
-    }
+    return (
+      <Content>
+        <form
+          className="form-signup text-center"
+          onSubmit={e => this.onSubmit(e)}
+        >
+          <img className="img-fluid mb-4" src={logo} alt="img" />
+          <h1 className="h3 mb-3 font-weight-normal">
+            Registro - Controle de Finanças
+          </h1>
+          <div className={status !== "" ? `alert alert-${status}` : ``}>
+            {message}
+          </div>
+          <div className="form-group">
+            <label htmlFor="name" className="sr-only">
+              Nome
+            </label>
+            <div className="input-group mb-2">
+              <div className="input-group-prepend">
+                <div className="input-group-text">
+                  <i className="fas fa-user"></i>
+                </div>
+              </div>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="form-control"
+                value={name}
+                placeholder="Nome"
+                required
+                onChange={e => this.setState({ name: e.target.value })}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email" className="sr-only">
+              E-mail
+            </label>
+            <div className="input-group mb-2">
+              <div className="input-group-prepend">
+                <div className="input-group-text">
+                  <i className="fas fa-envelope-square"></i>
+                </div>
+              </div>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="form-control"
+                value={email}
+                placeholder="E-mail"
+                required
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="sr-only">
+              Senha
+            </label>
+            <div className="input-group mb-2">
+              <div className="input-group-prepend">
+                <div className="input-group-text">
+                  <i className="fas fa-unlock-alt"></i>
+                </div>
+              </div>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="form-control"
+                value={password}
+                placeholder="Senha"
+                required
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="sr-only">
+              Confirmar Senha
+            </label>
+            <div className="input-group mb-2">
+              <div className="input-group-prepend">
+                <div className="input-group-text">
+                  <i className="fas fa-unlock-alt"></i>
+                </div>
+              </div>
+              <input
+                type="password"
+                id="password_confirmation"
+                name="password_confirmation"
+                value={password_confirmation}
+                className="form-control"
+                placeholder="Confirmar Senha"
+                required
+                onChange={e =>
+                  this.setState({ password_confirmation: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <button
+            className="btn btn-lg btn-primary btn-block"
+            type="submit"
+            disabled={!name || !email || !password || !password_confirmation}
+          >
+            Cadastrar <i className="fas fa-user-plus"></i>
+          </button>
+        </form>
+        <Footer className={"footer"} />
+      </Content>
+    );
+  }
 }
 
 export default Register;
